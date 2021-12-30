cd2ac7e381ccef672c97acee13974377d8d5bd76
unknown <a.gentes@hotmail.com>
2021-12-30 15:05:48 +0100

    MAJ 3 exo1

--- exo1.php
+++ exo1.php
@@ -1,4 +1,23 @@
 <!--TP : Trouver 2 manières différentes de retourner une string -->
 
+
+<!-- Méthode n°1 : -->
+
 <?php 
     echo strrev("J'adoooore les sushis !"); 
+?>
+
+<!-- Méthode n°2 : -->
+
+<?php
+function mb_strrev($str){
+    $r = '';
+    for ($i = mb_strlen($str); $i>=0; $i--) {
+        $r .= mb_substr($str, $i, 1);
+    }
+    return $r;
+}
+
+echo mb_strrev("J'adoooore les sushis !"); 
+?>
+    
